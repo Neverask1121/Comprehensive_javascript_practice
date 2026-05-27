563cb3902a4398b14f97226dd5b11969f3729ff7
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-05-27 17:36:42 +0530

    starting with arrays after this my lord

---
+++ Chapter-2/sandbox-2.js
@@ -0,0 +1,59 @@
+let radius = 10;
+const pi = 3.14;
+
+console.log(radius, pi);
+
+console.log(radius/3);
+console.log(radius%3);
+
+let puh = pi * radius**2;
+
+
+console.log(puh
+);
+
+
+
+//order of the calculation is BODMAS it seems
+
+
+let pussies = 10;
+
+pussies ++;
+pussies += 10
+console.log(pussies);
+
+
+
+// NaN is standing for not a number it seems
+
+console.log('puh' / 5);
+
+
+//concetenation shit again
+
+
+
+console.log('The girls has ' + pussies + ' pussies');
+
+
+
+//nigga is talking about template string now 
+// template string is a string that it lets you to inject string without the usage of pus sign to concatenate them
+
+console.log(`I am here to conquer her juicy ${juicypuh}`);
+
+
+//making use of the html template strings
+
+const author = 'Aditya';
+const book = 'Puhcific';
+let likes = 67;
+
+
+console.log(`
+        <h2>${author}</h2>
+        <p>Has created a book named</p>
+        <p>${book}</p>
+        <span>And it has these many likes ${likes}</span>
+    `);
--- Chapter-2/sandbox.js
+++ Chapter-2/sandbox.js
@@ -8,7 +8,7 @@ console.log(email);
 //String concatination
 let name = "Aditya";
 let surname = "Bhandari";
-console.log(name +" "+ surname);
+console.log(name + " " + surname);
 
 //getting characters
 console.log(surname[4]);
@@ -33,3 +33,31 @@ console.log(result, surname);
 
 let index = email.indexOf("g");
 console.log(index);
+
+
+//common string  methods
+let pusyy = email.lastIndexOf('n');
+console.log(pusyy);
+console.log(pusyy);
+
+//what slice does is it slices a section from the string
+
+let deck = email.slice(0, 9);
+console.log(deck);
+
+
+
+//substr takes to arguments one is the stating index and other is the number of index it wants to slice to
+let another = email.substr(4, 8);
+console.log(another);
+
+
+//using the method helps us do is that it replaces the first character in the string
+// and replaces it with the other one we argument
+
+let juicypuh = email.replace('n', 'p');
+console.log(juicypuh);
+
+
+
+
