cf7c2e1452d26d4810b6258473bb6a9c9f69df90
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-05-27 11:19:34 +0530

    Commit at 11:19

---
+++ Chapter-1/sandbox.js
@@ -0,0 +1,23 @@
+let year = 2008;
+console.log(1);
+console.log(2);
+let age = 25;
+console.log(age);
+console.log(age);
+console.log(year);
+age = 30;
+//logging things to the console
+/*this is my 
+interactive
+Javascript 
+Practice*/
+
+
+
+console.log(age);
+console.log(year, age);
+const points = 100;
+console.log(points);
+var pusSy = 75;
+pusSy = 69;
+console.log(pusSy);
