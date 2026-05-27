3abfd6d10be56b09b384e70d7b3474e1dd924d42
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-05-27 18:26:03 +0530

    adding some more changes

---
+++ Chapter-2/sandbox-3.js
@@ -0,0 +1,54 @@
+let ninjas = ['niggas', 'dont', 'lie'];
+console.log(ninjas[1]);
+
+
+let ages = [10, 20, 30, 40];
+console.log(ages[1]);
+
+console.log(ages);
+
+console.log(ages.length
+);
+
+console.log(ninjas.join(' '));
+console.log(ninjas.concat(['aditya', 'aman']));
+
+
+
+
+console.log(ninjas.push('aditya'));
+console.log(ninjas.pop());
+
+
+//the browser does is gives the default value as undefined
+
+
+let puheater = null;
+console.log(puheater, puheater+3, `the puh is ${puheater}`);
+
+//boolean functions
+
+console.log(true, false, "true", "false");
+console.log("true", "false");
+
+
+console.log(email.includes('!'));
+
+
+console.log(ninjas.includes('nigga'));
+
+
+//some comparisons
+
+console.log(pussies > 10);
+console.log(pussies <= 10);
+console.log(pussies < 10);
+console.log(pussies >= 10);
+console.log(pussies != 10);
+console.log(pussies == 10);
+
+//some comparisons agian
+console.log('Nigga' > 'chigga');
+//here only the first letter is compared
+
+
---
+++ Chapter-2/sandbox-4.js
@@ -0,0 +1,15 @@
+//this is the automatic type conversion that is the backend process of the javascript compiler
+
+
+
+let nigga = 25;
+console.log(nigga == 25);
+console.log(nigga == '25');
+
+
+//if you want to do the strict comparison then you include is that the ===
+// or we can use !==
+
+
+console.log(nigga === 25);
+console.log(nigga === '25');
