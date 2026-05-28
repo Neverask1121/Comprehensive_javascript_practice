9ee2dd1bfeb0efd05b9b8564fdf4c20222faec80
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-05-28 14:51:03 +0530

    Adding chapter-3

--- Chapter-2/sandbox-4.js
+++ Chapter-2/sandbox-4.js
@@ -13,3 +13,35 @@ console.log(nigga == '25');
 
 console.log(nigga === 25);
 console.log(nigga === '25');
+
+//Data type conversion doing
+
+let score =  '69';
+//doing the type conversion
+score = Number(score);
+console.log(score + 1);;
+console.log(typeof score);
+
+
+let kerk = 'hello';
+kerk = Number(kerk);
+console.log(typeof kerk);
+console.log(kerk+1);
+
+//number to string type conversion
+
+console.log(String(50));
+let testicles = String(50);
+console.log(typeof testicles);
+
+//Integer to boolean conversion
+console.log(Boolean(0));
+
+
+//String to boolean conversion what happens
+
+console.log(Boolean('hello'));
+
+console.log(Boolean(''));
+
+//This is all Explicit type conversion
---
+++ Chapter-3/sandbox.js
@@ -0,0 +1,14 @@
+//practice for loops
+
+for(let i = 0 ; i < 5 ; i++){
+    console.log('Modih', i);
+}
+console.log('Meloni');
+
+//Through the string
+
+const names = ['Modi', 'Meloni', 'Mamta'];
+
+for(let i = 0 ; i < names.length ; i++){
+    
+}
