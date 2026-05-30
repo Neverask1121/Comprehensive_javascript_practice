35bc9a5bb04f0b72dc527599f618bd48b3c5a6c3
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-05-31 01:10:44 +0530

    Almost nding chapter-3

--- Chapter-3/sandbox.js
+++ Chapter-3/sandbox.js
@@ -35,3 +35,29 @@ else{
 
 //Starting logical operators
 
+const password = 'password@1234';
+
+if(password.length >= 12 && password.includes('@')){
+    console.log('You might be true');
+}
+
+
+console.log(!true);
+console.log(!true);
+
+
+const scores = [10,20,30,40,50,60,70,80,90];
+
+for(let i = 0 ; i < scores.length ; i++){
+    if(scores[i] === 40){
+        continue;
+    }
+    else if(scores[i] === 80){
+        break;
+    }
+    console.log(scores[i]);
+}
+
+
+
+
