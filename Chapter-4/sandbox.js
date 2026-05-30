35e94cb81f8fde9daed86ef23a061d354a24e31f
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-05-31 01:46:47 +0530

    Adding chapter-4 to the gng

---
+++ Chapter-4/sandbox.js
@@ -0,0 +1,26 @@
+//function declaration
+
+console.log('check');
+
+
+//function expression
+function nigga(){
+    console.log('pussy');
+
+}
+
+nigga();
+nigga();
+nigga();
+
+
+//function expression
+
+const speak = function(){
+    console.log('this works niga');
+};
+
+speak();
+speak();
+
+
