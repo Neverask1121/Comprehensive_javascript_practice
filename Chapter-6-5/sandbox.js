f21b423c133599f38a5e742e781bbc5a741451c7
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-04 01:38:06 +0530

    Adding Chapter-6-5

---
+++ Chapter-6-5/sandbox.js
@@ -0,0 +1,7 @@
+const copy = document.querySelector('.copy-me');
+
+copy.addEventListener('copy', () => {
+    console.log('Nigga dont copy orlese your wife will come to my room');
+});
+
+const mouse = document.querySelector('.')
