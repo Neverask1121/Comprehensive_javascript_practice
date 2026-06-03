a12df5ae039cc6d86d22814a9d456da86a74709a
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-03 11:09:13 +0530

    End of the first series of lecture of javascript course on yt

--- Chapter-6-1/sandbox.js
+++ Chapter-6-1/sandbox.js
@@ -19,3 +19,24 @@ console.log(link.classList); // getting the list of classes in the p class
 link.classList.add('error'); // adding another class to them 
 link.classList.remove('error'); // removing the class from the list of classes
 link.classList.add('success'); // adding the success class to the list of classes
+
+
+// challenge question
+
+
+const link2 = document.querySelectorAll('p');
+link2.forEach(element => {
+    if(element.textContent.includes('error')){
+        element.classList.add('error');
+    }
+    else if(element.textContent.includes('success')){
+        element.classList.add('success');
+    }
+});
+
+
+// now we learn about how to toggle classes
+
+const hello = document.querySelector(".title");
+hello.classList.toggle('test');
+hello.classList.toggle('test');
