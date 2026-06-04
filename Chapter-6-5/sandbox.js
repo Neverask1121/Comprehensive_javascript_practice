8f928ebd661c5a025c71489c3d7d4137807fa436
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-04 11:16:26 +0530

    wrapping up Chapter-6-5

--- Chapter-6-5/sandbox.js
+++ Chapter-6-5/sandbox.js
@@ -4,4 +4,17 @@ copy.addEventListener('copy', () => {
     console.log('Nigga dont copy orlese your wife will come to my room');
 });
 
-const mouse = document.querySelector('.')
+const mouse = document.querySelector('.box');
+mouse.addEventListener('mousemove', e => {
+    //console.log(e);
+    // to output coordinates inside the box
+    // console.log(e.offsetX, e.offsetY);
+    // now changing the box content everytime we move
+    mouse.textContent = `x - ${e.offsetX} y - ${e.offsetY}`;
+});
+
+// scroll eventListener
+
+document.addEventListener('wheel', e => {
+    console.log(e.pageX, e.pageY);
+})
