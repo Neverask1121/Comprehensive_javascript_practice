9b760baa3e6cd07b4c7a57f20fecbbf8a62c3eec
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-04 21:37:38 +0530

    Completing the quizes project

--- Quiz-Project/app.js
+++ Quiz-Project/app.js
@@ -1,9 +1,10 @@
 const correctAnswers = ['B', 'B', 'B', 'B', 'B'];
 const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
 
+let score = 0;
 form.addEventListener('submit', e => {
     e.preventDefault(); // Default action is to refresh the page and we do not want that shit to happen
-    let score = 0;
     const userAnswers = [form.q1.value, form.q1.value, form.q3.value, form.q4.value, form.q5.value];
 
     // Now comparing the answers to actual answers
@@ -13,5 +14,24 @@ form.addEventListener('submit', e => {
             score += 20;
         }
     });
+    scrollTo(0, 0);
     console.log(score);
 });
+
+
+// Learning about the window object discovering something good
+
+setTimeout(() => {
+    alert('Why are you gay?');
+}, 4000);
+
+let output =0;
+setInterval(() => {
+    if(output === score){
+        clearInterval();
+    }
+    else{
+        output++;
+        result.querySelector('span').textContent = `${output}%`;
+    }
+}, 10);
