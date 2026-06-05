3996c700e7e6aed171817f43b39b39e643a5d867
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-05 09:26:21 +0530

    Adding till map method

--- Chapter-9/sandbox.js
+++ Chapter-9/sandbox.js
@@ -1,22 +1,57 @@
-const scores = [10,20,30,40,50, 1, 2, 3];
+// const scores = [10,20,30,40,50, 1, 2, 3];
 
-//Array methods
+// //Array methods
 
-const filtered = scores.filter((score)=>{
-    return score > 20;
-})
-console.log(filtered);
+// const filtered = scores.filter((score)=>{
+//     return score > 20;
+// })
+// console.log(filtered);
 
 
-const users = [
-    {name: 'aditya', premium: false},
-    {name: 'ana', premium: true},
-    {name: 'mia', premium: true},
-    {name: 'ginger', premium: false},    
-];
+// const users = [
+//     {name: 'aditya', premium: false},
+//     {name: 'ana', premium: true},
+//     {name: 'mia', premium: true},
+//     {name: 'ginger', premium: false},    
+// ];
 
-const premiumUsers = users.filter((name)=>{
-    return name.premium;
-});
+// const premiumUsers = users.filter((name)=>{
+//     return name.premium;
+// });
 
-console.log(premiumUsers);
+// console.log(premiumUsers);
+
+
+// // map methods
+// const scores = [10,20,30,40,50,60,70];
+// const map_method = scores.map((res) => {
+//     return res / 2;
+// });
+// console.log(map_method);
+
+// harder
+
+// const products = [
+//     {name: 'Ayesha', rate: '60'},
+//     {name: 'Alina', rate: '67'},
+//     {name: 'Zelina', rate: '800'},
+//     {name: 'Subhi', rate: '900'}
+// ];
+
+// const products_half = products.map((result) =>{
+//     if(result.rate > 100){
+//         return {name: result.name, rate: result.rate / 2};
+//     }
+//     else{
+//         return result;
+//     }
+// });
+
+// console.log(products_half);
+// // original array stays intact
+// console.log(products);
+
+
+// Reduce method
+
+const 
