be3cd60a537c4cf5d826039a69df022a597e42eb
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-08 19:26:46 +0530

    Pre goon commit

--- Chapter-12/script.js
+++ Chapter-12/script.js
@@ -1,9 +1,37 @@
-console.log(1);
+// console.log(1);
 
-setTimeout(() => {
-  console.log("Hello Callback function is fired....");
-}, 2000);
-console.log(1);
-console.log(1);
-console.log(1);
-console.log(1);
+// setTimeout(() => {
+//   console.log("Hello Callback function is fired....");
+// }, 2000);
+// console.log(1);
+// console.log(1);
+// console.log(1);
+// console.log(1);
+
+
+
+// Making a http request
+
+const request = new XMLHttpRequest();
+
+// logging the request state change in the console
+
+request.addEventListener('readystatechange', () => {
+  // console.log(request, request.readyState);
+  // we can do something only at this stage
+  if(request.readyState === 4 && request.status == 200){
+    console.log(request, request.responseText);
+  }
+});
+
+
+// to open the request here
+
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+
+
+
+// now actually sending the request
+
+request.send();
